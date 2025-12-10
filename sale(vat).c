2fe8d09d722d1e4a87cccaac6f192be93cb4a070
6704062616010 สรัล รัตนศักดิ์ <s6704062616010@email.kmutnb.ac.th>
2025-12-10 13:37:43 +0700

    calculate VAT

---
+++ sale(vat).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    float goods1,goods2,goods3,total1,total2,total3;
+   
+    scanf("%f",&goods1);
+    total1 = (goods1*0.95)*1.07;
+    printf("%.2f\n",total1);
+
+    scanf("%f",&goods2);
+    total2 = ((goods1+goods2)*0.85)*1.07;
+    printf("%.2f\n",total2);
+
+    scanf("%f",&goods3);
+    total3 = ((goods1+goods2+goods3)*0.7)*1.07;
+    printf("%.2f\n",total3);
+
+}
